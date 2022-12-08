35ad9e2a175c7829b474e5ece0a37252692a94a2
Moringa School <lizdmukami@gmail.com>
2022-12-08 17:15:38 +0300

    updated the repos Button

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 const init = () => {
 
-const ul = document.getElementById('user-list');
-const listUser = document.createElement('li');
-const listRepo = document.createElement('li');
 const inputForm = document.querySelector('form');
 const userlist = document.getElementById('user-list')
 const reposlist = document.getElementById('repos-list')
@@ -28,46 +25,55 @@ fetch(url, {
   .then((data) => {
     let githubUsers = data;
 
-    //   For each githubUser in githubUsers, create a list item that displays their name
-    function loop() {
-    githubUsers.forEach(renderUsers);
-    }
-    
+      // For each githubUser in githubUsers, create a list item that displays their name
+    githubUsers.items.forEach( (githubUser) =>{
 
-    function renderUsers(githubUser) {
-        let liUser = document.createElement('li');
-        let liRepo = document.createElement('li');
         let username = document.createElement('h2');
-        let avatar = document.createElement('span');
-        let link = document.createElement('span');
+        let avatar = document.createElement('div');
+        let link = document.createElement('p');
+        let repositories = document.createElement('button')
 
         // using string interpolation to add the username, avatar and link
         username.innerHTML = `${githubUser.login}`;
-        avatar.innerHTML = `${githubUser.avatar_url}`;
+        avatar.innerHTML =`<img src = "${githubUser.avatar_url}"/>`;
         link.innerHTML = `${githubUser.html_url}`;
+        repositories.innerHTML = "Repositories";
+        repositories.style='margin-bottom: 10px'
+        avatar.style='margin-bottom: 50px, width: 50px, height: 50px'
 
-        // Add the users repository to the DOM
-        let repos = document.getElementById('repos-list');
-        repos.addEventListener('click', () => {
-            liRepo.innerHTML = `${githubUser.repos_url}`;
+
+        userlist.appendChild(username);
+        userlist.appendChild(link);
+        userlist.appendChild(repositories);
+        userlist.appendChild(avatar);
+
+                   // Add the users repository to the DOM
+                   
+                   repositories.addEventListener('click', () => {
+                     fetch(githubUser.repos_url, {
+                       method: 'GET',
+                       header:{
+                            'Content-Type': 'application/json',
+                             Accept: 'application/vnd.github.v3+json'
+                             }
+                             })
+                               .then(response => response.json())
+                                   .then(data => { data.forEach(githubUser => {
+                                       let repoCard = document.createElement('li')
+                                       repoCard.innerHTML = `
+                                           <h4> ${githubUser.name} </h4>
+                                           <p> ${githubUser.html_url}</p>
+                                       `
+                                       reposlist.appendChild(repoCard)    
+                                   });
+                               });
+                         })
         })
-        .catch(function(error) {
-            console.log(error);
-          });
+      })
 
-        // connect these DOM elements with appendChild
-        li.appendChild(username);
-        li.appendChild(avatar);
-        li.appendChild(link);
-        listRepo.appendChild(liRepo);
-        listUser.appendChild(liUser);
-    }
-      });
-  })
+    });
+ 
+  }
 };
-
-  userlist.appendChild(listUser);
-  reposlist.appendChild(listRepo);
-}
 
   document.addEventListener('DOMContentLoaded', init);
